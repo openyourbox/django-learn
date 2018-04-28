f385d9491cceda840df5c11977318bbc533cb414
suzumiya <suzumiya@openmailbox.org>
2018-04-28 16:31:18 +0800

    tutorial03

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail, name = 'detail'),
+    path('<int:question_id>/results/', views.result),
+    path('<int:question_id>/vote/', views.vote, name = 'vote')
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse('Hello h来了')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
     # return render(request, 'index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s.' %question_id)
+
+def result(request, question_id):
+    response = 'You are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('You are voting on question %s.' %question_id)
+
